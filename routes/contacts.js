04c4cc0107de2aa8ce8c614a95595e0a08f38420
Sofonias Abathun <sofoniasabathun@gmail.com>
2025-09-18 18:18:17 +0300

    refactor: update findContacts method and improve contacts retrieval in routes

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,32 +1,62 @@
 const createError = require('http-errors');
 const { status: httpStatus } = require('http-status');
 const express = require('express');
-const { id, validateRequest, searchTerm, page, pageSize, sortBy, sortOrder } = require('../validators');
+const {
+  id,
+  validateRequest,
+  searchTerm,
+  page,
+  pageSize,
+  sortBy,
+  sortOrder,
+} = require('../validators');
 const db = require('../models');
 const { MODEL_NAMES, REQUEST_PAYLOAD_LOCATION } = require('../CONST');
 
 const router = express.Router();
 
+const getContactsValidators = [searchTerm, page, pageSize, sortBy, sortOrder];
+
 /* GET contacts listing. */
 router.get(
-  '/:userId',
-  [
-    id(REQUEST_PAYLOAD_LOCATION.PARAMS, 'userId'),
-    searchTerm,
-    page,
-    pageSize,
-    sortBy,
-    sortOrder,
-  ],
+  '/',
+  getContactsValidators,
   validateRequest,
   async function(req, res, next) {
     try {
       const { searchTerm, page, pageSize, sortBy, sortOrder } = req.query;
-      const userProfile = await db[MODEL_NAMES.USER_PROFILE].findByUserId(req.params.userId);
+      const { count: total, rows: contacts } = await db[
+        MODEL_NAMES.LOG_BOOK_CONTACT
+      ].findContacts({
+        searchTerm,
+        page,
+        pageSize,
+        sortBy,
+        sortOrder,
+      });
+      res.json({ total, contacts });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.get(
+  '/:userId',
+  getContactsValidators.concat(id(REQUEST_PAYLOAD_LOCATION.PARAMS, 'userId')),
+  validateRequest,
+  async function(req, res, next) {
+    try {
+      const { searchTerm, page, pageSize, sortBy, sortOrder } = req.query;
+      const userProfile = await db[MODEL_NAMES.USER_PROFILE].findByUserId(
+        req.params.userId
+      );
       if (!userProfile) {
         throw createError(httpStatus.NOT_FOUND, 'User not found');
       }
-      const contacts = await db[MODEL_NAMES.LOG_BOOK_CONTACT].findContactsByUserId({
+      const { count: total, rows: contacts } = await db[
+        MODEL_NAMES.LOG_BOOK_CONTACT
+      ].findContacts({
         userId: userProfile.id,
         searchTerm,
         page,
@@ -35,9 +65,12 @@ router.get(
         sortOrder,
       });
       if (!contacts || contacts.length === 0) {
-        throw createError(httpStatus.NOT_FOUND, 'No contacts found for this user');
+        throw createError(
+          httpStatus.NOT_FOUND,
+          'No contacts found for this user'
+        );
       }
-      res.json(contacts);
+      res.json({ total, contacts });
     } catch (error) {
       next(error);
     }
